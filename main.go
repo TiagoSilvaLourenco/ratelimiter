59c40c5cbedf731cf3fa8e1e17f6860432bbe675
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2024-01-24 22:39:22 -0300

    get ip and token

--- main.go
+++ main.go
@@ -20,10 +20,24 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.Use(rateLimiterMiddleware)
 	r.GET("/api/resource", handleRequest)
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func handleRequest(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Request successful"})
+}
+
+func rateLimiterMiddleware(c *gin.Context) {
+
+	// get ip address of client
+	ip := c.ClientIP()
+
+	// get token of access from header
+	token := c.GetHeader("API_KEY")
+
+	println("IP: ", ip)
+	println("Token: ", token)
+
 }
