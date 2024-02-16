81a4f1f868dc7f7f3dc5c8453c16b2cd41da6488
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2024-02-15 22:07:33 -0300

    feature: rate limiter, pattern strategy, fix cors

--- main.go
+++ main.go
@@ -14,8 +14,23 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var ctx = context.Background()
-var client *redis.Client
+var (
+	ctx         = context.Background()
+	persistence Persistence
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	persistence = Persistence(&RedisPersistence{client: redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})})
+}
 
 type RateLimiterConfig struct {
 	Limit     int
@@ -24,81 +39,21 @@ type RateLimiterConfig struct {
 	UseToken  bool
 }
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	client = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+type Persistence interface {
+	GetLimit(key string, defaultLimit int) (int, error)
+	Incr(key string) (int64, error)
+	Expire(ctx context.Context, key string, duration time.Duration) error
 }
 
-func main() {
-	r := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://resttesttest.com"}
-	config.AllowHeaders = []string{"Content-Type", "API_KEY"}
-	config.AllowMethods = []string{"GET"}
-	config.AllowCredentials = true
-
-	r.Use(cors.New(config))
-	r.Use(rateLimiterMiddleware())
-
-	r.GET("/api/resource", handleRequest)
-	r.Run(":8080")
+type RedisPersistence struct {
+	client *redis.Client
 }
 
-func handleRequest(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Request successful"})
-}
+func (r *RedisPersistence) GetLimit(key string, defaultLimit int) (int, error) {
 
-func rateLimiterMiddleware() gin.HandlerFunc {
-
-	return func(c *gin.Context) {
-		var key string
-		var limit int
-		blockTime := time.Second
-
-		if c.GetHeader("API_KEY") != "" {
-			key = "token:" + c.GetHeader("API_KEY")
-			limit = getEnvInt("TOKEN_LIMIT", 100)
-
-		} else {
-			key = "ip:" + c.ClientIP()
-			limit = getEnvInt("IP_LIMIT", 10)
-			blockTime = blockTime * 2
-		}
-
-		limit, err := getLimit(key, limit)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-			return
-		}
-		count, _ := client.Incr(ctx, "count:"+key).Result()
-		client.Expire(ctx, "count:"+key, blockTime)
-
-		log.Println("count: ", count)
-		log.Println("key: ", key)
-		log.Println("limit: ", limit)
-
-		if count > int64(limit) {
-			c.JSON(http.StatusTooManyRequests, gin.H{"message": "You have reached the maximum number of requests or actions allowed within a certain time frame"})
-			c.Abort()
-			return
-		}
-
-		c.Next()
-	}
-}
-
-func getLimit(key string, defaultLimit int) (int, error) {
+	client := r.client
 	limitStr, err := client.Get(ctx, key).Result()
-	// log.Println("Limit: ", limitStr)
+
 	if err == redis.Nil {
 		client.Set(ctx, key, strconv.Itoa(defaultLimit), 0)
 		return defaultLimit, nil
@@ -122,6 +77,76 @@ func getLimit(key string, defaultLimit int) (int, error) {
 		return 0, err
 	}
 	return defaultLimit, nil
+}
+
+func (r *RedisPersistence) Incr(key string) (int64, error) {
+	count, _ := r.client.Incr(ctx, "count:"+key).Result()
+	return count, nil
+}
+
+func (r *RedisPersistence) Expire(ctx context.Context, key string, duration time.Duration) error {
+	r.client.Expire(ctx, key, duration)
+	return nil
+}
+
+func main() {
+	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://resttesttest.com"}
+	config.AllowHeaders = []string{"Content-Type", "API_KEY"}
+	config.AllowMethods = []string{"GET"}
+	config.AllowCredentials = true
+
+	r.Use(cors.New(config))
+
+	r.Use(rateLimiterMiddleware(persistence))
+
+	r.GET("/api/resource", handleRequest)
+	r.Run(":8080")
+}
+
+func handleRequest(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Request successful"})
+}
+
+func rateLimiterMiddleware(persistence Persistence) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		var key string
+		var limit int
+		blockTime := time.Second
+
+		if c.GetHeader("API_KEY") != "" {
+			key = "token:" + c.GetHeader("API_KEY")
+			limit = getEnvInt("TOKEN_LIMIT", 100)
+
+		} else {
+			key = "ip:" + c.ClientIP()
+			limit = getEnvInt("IP_LIMIT", 10)
+			blockTime = blockTime * 2
+		}
+
+		limit, err := persistence.GetLimit(key, limit)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		count, _ := persistence.Incr(key)
+		persistence.Expire(ctx, "count:"+key, blockTime)
+
+		log.Println("count: ", count)
+		log.Println("key: ", key)
+		log.Println("limit: ", limit)
+
+		if count > int64(limit) {
+			c.JSON(http.StatusTooManyRequests, gin.H{"message": "You have reached the maximum number of requests or actions allowed within a certain time frame"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
 }
 
 func getEnvInt(key string, defaultValue int) int {
